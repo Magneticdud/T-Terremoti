275d2a9ce7217e41f2f43ff992b670aa08e42882
Magnetic_dud <mail@dandandin.it>
2017-12-30 21:02:22 +0100

    come perdere tempo
    perché fare una tabella precompilata in 5 minuti quando si può trovare una funzione logaritmica che funziona pure male in 2 ore?

--- app/src/main/java/com/example/android/quakereport/TerremAdapter.java
+++ app/src/main/java/com/example/android/quakereport/TerremAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,7 +11,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+
+import static java.lang.Math.log;
 
 public class TerremAdapter extends ArrayAdapter<Terremoto> {
     public TerremAdapter(Activity context, ArrayList<Terremoto> terremoti) {
@@ -37,7 +42,9 @@ public class TerremAdapter extends ArrayAdapter<Terremoto> {
         TextView magnitudoTextView = (TextView) listItemView.findViewById(R.id.magnitudo);
         // Get the version name from the current AndroidFlavor object and
         // set this text on the name TextView
-        magnitudoTextView.setText(String.valueOf(currenTerremoto.getMagnitudo()));
+        DecimalFormat formatter = new DecimalFormat("0.0");
+        String convertito = formatter.format(currenTerremoto.getMagnitudo());
+        magnitudoTextView.setText(convertito);
 
         // Find the TextView in the list_item.xml layout with the ID meaning
         TextView cittaTextView = (TextView) listItemView.findViewById(R.id.citta);
@@ -51,7 +58,47 @@ public class TerremAdapter extends ArrayAdapter<Terremoto> {
         TextView dataTextView = (TextView) listItemView.findViewById(R.id.data);
         dataTextView.setText(currenTerremoto.getNiceData());
 
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudoTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currenTerremoto.getMagnitudo());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
         // Return the whole list item layout so that it can be shown in the ListView
         return listItemView;
+    }
+
+    private int getMagnitudeColor(double magnitudo){
+        //https://stackoverflow.com/questions/340209/generate-colors-between-red-and-green-for-a-power-meter
+        //allora: magnitudo fino a 2, H tra 0.5 e 0.6
+        //magnitudo fino a 3 si va a 0.4
+        //magnitudo fino a 4 si va a 0.2
+        //magnitudo di 10 si va a 0.0
+        //se superiore a 8 brightness meno .3
+        //curva che passa dai punti {{0,0.6},{2,0.5},{3,0.4},{4,0.2},{6,0.1},{10,0}}
+        // -0.3*log(0.1*magnitudo)
+        float[] HSV = new float [3];
+        double H = -0.2*log(0.1*magnitudo)*360;
+        if (H>=0.6*360) {
+            //diventa blu
+            H = 215;
+        } else if (H<=0) {
+            //diventa rosso
+            H = 0;
+        }
+        HSV[0] = (float)H;
+        HSV[1] = (float)0.9; // Saturation
+        HSV[2] = (float)0.9; // Brightness
+        if (magnitudo>7.0) {
+            HSV[2] = HSV[2]-(float)0.3;
+        }
+        int colore = Color.HSVToColor(HSV);
+
+        return colore;
     }
 }
